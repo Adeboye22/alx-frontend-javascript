e4ba2837719957800430c28a51b35a19672c6818
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2022-12-23 23:17:24 +0000

    Promises in CART.

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,9 @@
+mport { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    return { photo: await uploadPhoto(), user: await createUser() };
+  } catch (e) {
+    return { photo: null, user: null };
+  }
+}

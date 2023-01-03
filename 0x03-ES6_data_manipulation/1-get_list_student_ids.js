fcc8a2cfdb6fae7899dd7404cc913992fd82f156
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2023-01-03 22:59:18 +0000

    More mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,3 @@
-/*Create a function getListStudentIds that returns an array of Ids from a list of object*/
 export default function getListStudentIds(lst) {
   if (Array.isArray(lst)) {
     return lst.map((obj) => obj.id);
